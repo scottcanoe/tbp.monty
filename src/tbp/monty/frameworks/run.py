08db4873bb61c0a7463cb945ff0a355a5b5ef69d
Scott Knudstrup <scottcanoe@users.noreply.github.com>
2025-11-23 19:52:27 -0500

    add yaml files

--- src/tbp/monty/frameworks/run.py
+++ src/tbp/monty/frameworks/run.py
@@ -14,7 +14,7 @@ import pprint
 import time
 
 import hydra
-from omegaconf import DictConfig
+from omegaconf import DictConfig, OmegaConf
 
 from tbp.monty.hydra import register_resolvers
 
@@ -26,7 +26,7 @@ def print_config(config):
     print("\n\n")
     print("Printing config below")
     print("-" * 100)
-    print(pprint.pformat(config))
+    print(OmegaConf.to_yaml(config))
     print("-" * 100)
 
 
